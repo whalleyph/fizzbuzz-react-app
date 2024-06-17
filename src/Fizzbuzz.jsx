c957bcb9cf708b950291960ee6199b3aad552b8e
2405-011-pw <walypa56@gmail.com>
2024-06-17 15:46:07 +0100

    fix async result

--- src/Fizzbuzz.jsx
+++ src/Fizzbuzz.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Fizzbuzz() {
   const [fizzBuzzNumber, setFizzBuzzNumber] = React.useState(1);
-  const [fizzBuzzResult, setFizzBuzzResult] = React.useState(1)
+  const [fizzBuzzResult, setFizzBuzzResult] = React.useState(1);
 
   function increaseFizzBuzzNumber() {
     setFizzBuzzNumber((number) => number + 1);
-    setFizzBuzzResult(generateFizzBuzz(fizzBuzzNumber))
+    setFizzBuzzResult(generateFizzBuzz(fizzBuzzNumber + 1));
   }
 
   function resetFizzBuzzNumber() {
-      setFizzBuzzNumber(1);
-      setFizzBuzzResult(1);
+    setFizzBuzzNumber(1);
+    setFizzBuzzResult(1);
   }
 
   function generateFizzBuzz(number) {
