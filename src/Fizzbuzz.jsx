10cd1774bfa8db0575b14d1d6b4f389c3a36ee9b
2405-011-pw <walypa56@gmail.com>
2024-06-17 16:12:50 +0100

    removed state for result

--- src/Fizzbuzz.jsx
+++ src/Fizzbuzz.jsx
@@ -2,28 +2,16 @@ import React from "react";
 
 export default function Fizzbuzz() {
   const [fizzBuzzNumber, setFizzBuzzNumber] = React.useState(1);
-  const [fizzBuzzResult, setFizzBuzzResult] = React.useState(1);
 
   function increaseFizzBuzzNumber() {
     setFizzBuzzNumber((number) => number + 1);
-    setFizzBuzzResult(generateFizzBuzz(fizzBuzzNumber + 1));
   }
 
   function resetFizzBuzzNumber() {
     setFizzBuzzNumber(1);
-    setFizzBuzzResult(1);
   }
 
-  function generateFizzBuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-      number = "FizzBuzz";
-    } else if (number % 3 === 0) {
-      number = "Fizz";
-    } else if (number % 5 === 0) {
-      number = "Buzz";
-    }
-    return number;
-  }
+  const fizzBuzzResult = generateFizzBuzz(fizzBuzzNumber)
 
   return (
     <>
@@ -32,4 +20,15 @@ export default function Fizzbuzz() {
       <button onClick={resetFizzBuzzNumber}>Reset</button>
     </>
   );
+}
+
+function generateFizzBuzz(number) {
+  if (number % 15 === 0) {
+    number = "FizzBuzz";
+  } else if (number % 3 === 0) {
+    number = "Fizz";
+  } else if (number % 5 === 0) {
+    number = "Buzz";
+  }
+  return number;
 }
